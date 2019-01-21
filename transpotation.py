da4dc637d3493f31e9ae00c37be8c1ba6268ac11
rainyuxia0112 <44214595+rainyuxia0112@users.noreply.github.com>
2019-01-20 19:25:46 -0800

    Update transpotation.py

--- transpotation.py
+++ transpotation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 data=pd.read_csv('/Users/rain/Desktop/datapreprocessing.csv')
-
 #missing data
 data.drop(data.index[[33,38]],inplace=True)
+
 
 Y=data.iloc[:,1].values
 
@@ -34,36 +34,59 @@ X=np.delete(X,0,axis=1)
 
 
 X = np.append(arr=np.ones((70,1)).astype(int), values=X, axis=1)   # 把b0 也变成 b0*x0
+# backward method  (找最后使用参数！）
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(Y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+X= backwardElimination(X,0.1)
+
+
 from sklearn.model_selection import train_test_split
 #x为数据集的feature熟悉，y为label.
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
 
-
-# backward method  (找最后使用参数！）
-import statsmodels.formula.api as sm
-X_opt = x_train[:,:]
-regressor_OLS = sm.OLS(endog=y_train,exog=X_opt.astype(float)).fit()
-regressor_OLS.summary()
-
-
-X_opt = X_opt[:,[0,1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17]]
-regressor_OLS = sm.OLS(endog=y_train,exog=X_opt.astype(float)).fit()
-regressor_OLS.summary()
-X_opt = X_opt[:,[0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16]]
-regressor_OLS = sm.OLS(endog=y_train,exog=X_opt.astype(float)).fit()
-regressor_OLS.summary()
-X_opt = X_opt[:,[0,1,2,3,5,6,7,8,9,10,11,12,13,14,15]]
-regressor_OLS = sm.OLS(endog=y_train,exog=X_opt.astype(float)).fit()
-regressor_OLS.summary()
-X_opt = X_opt[:,[0,1,2,4,5,6,7,8]]
-
-
-
-# prediction
-regressor_OLS.params
-y_prediction=regressor_OLS.predict(x_test[:,[0,1,6,11,12,13,14,15]])
+#model
+from sklearn.linear_model import LinearRegression
+reg=LinearRegression()
+reg.fit(x_train,y_train)
+y_pre=reg.predict(x_test)
 from sklearn.metrics import r2_score
-r2_score(y_true=y_test,y_pred=y_prediction)
+r2_score(y_true=y_test,y_pred=y_pre)
 
 
-#0.8949873051103879
+
+# using SVR to predict
+
+# using PCA to choose peramater
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+X_ = sc_x.fit_transform(X)
+sc_y = StandardScaler()
+Y_ = sc_y.fit_transform(Y.reshape(-1,1))
+from  sklearn.decomposition import PCA
+pca=PCA(n_components=10)
+X_new=pca.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+#x为数据集的feature熟悉，y为label.
+x_train, x_test, y_train, y_test = train_test_split(X_new, Y_, test_size = 0.3)
+
+
+from sklearn.svm import SVR
+reg=SVR(kernel='rbf',degree=16,C=6)
+reg.fit(x_train,y_train)
+y_pre=reg.predict(x_test)
+from sklearn.metrics import r2_score
+r2_score(y_true=y_test,y_pred=y_pre)
+
+
+
