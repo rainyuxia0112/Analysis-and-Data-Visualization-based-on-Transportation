2e2aaa573e4966a3a30f5d410919281d90091fb3
rainyuxia0112 <44214595+rainyuxia0112@users.noreply.github.com>
2019-03-29 22:18:50 -0700

    Update mid200b.py

--- mid200b.py
+++ mid200b.py
@@ -1,26 +1,28 @@
+""" this project aims to use la transpotation data to regress time with other predictors
+"""
+
+### import data into dataframe
 import pandas as pd
 data=pd.read_csv('/Users/rain/Desktop/data200b.csv')
 Y=data.iloc[:,1].values
 X=data.iloc[:,[0,3,4,5,6,7]]
 
-# plots for features
-# distance
+### one dimension plot for different predictors in X
+# distplot of distance
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.distplot(X['distance'])
-# transport
+# pie plot for transport
 sizes=[len(X[X['Transport']=='walk']),len(X[X['Transport']=='bus']),len(X[X['Transport']=='bike']),
        len(X[X['Transport']=='ride share']), len(X[X['Transport']=='car']), len(X[X['Transport']=='ride share and bus'])]
-
 labels = 'walk','bus','bike','ride share','car', 'ride share and bus'
-explode = (0, 0.1,0,0,0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+explode = (0, 0.1,0,0,0,0)  
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 plt.show()
 
-# weekday
+# pie plot for weekday
 sizes=[len(X[X['Day_of_week']=='mon']),len(X[X['Day_of_week']=='tue']),len(X[X['Day_of_week']=='wed']),len(X[X['Day_of_week']=='thr']),len(X[X['Day_of_week']=='fri'])]
 labels= ['mon','tue','wed','thr','fri']
 explode = (0, 0.1,0,0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
@@ -39,18 +41,20 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 plt.show()
 
-# Time_crunch
+# pie plot for Time_crunch
 sizes=[len(X[X['Time_crunch']==1]),len(X[X['Time_crunch']==0])]
 labels = 'Time_crunch','not Time_crunch'
-explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+explode = (0, 0.1)  
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 plt.show()
 
-# change departure time to datetime
+# hist plot for departure time
 from datetime import datetime
+"""
+this loop aims to change the departure time to catego
 for i in range(len(X)):
     X.iloc[:, -1][i]=datetime.strptime(X.iloc[:, -1][i], "%H:%M")
 # plot hist for departuretime
@@ -60,9 +64,7 @@ plt.ylabel('number')
 plt.xlabel('hour for departure')
 plt.show()
 
-
-
-# build the model to SLR
+### use each of predictors in X to build the model to SLR
 # use distance to build SLR
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -74,10 +76,12 @@ est = sm.OLS(Y, dis)
 est2 = est.fit()
 est2.summary()
 
-
-# build a SLR to category features ( weekday, transport, rain , chrunch)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 def SLR(i):
-    from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+"""
+this function is to use one to predictors(catog) in X to buile SLR
+i -- index of the col in X
+return summary of the SLR"""
     labelencoder_X = LabelEncoder()
     x = np.array(X.iloc[:, i])
     x = labelencoder_X.fit_transform(x)
@@ -94,7 +98,3 @@ SLR(2)
 SLR(3)
 SLR(4)
 
-# change departime to time 
-X.iloc[:,5]=time
-
-SLR(5)
